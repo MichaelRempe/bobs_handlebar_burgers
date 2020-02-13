e49ffe07a44e29df607c7f700c35d9c620b4ab70
MichaelRempe <michaeljrempe@gmail.com>
2020-02-13 15:47:53 -0500

    remove uneeded requirments

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,8 +1,5 @@
 // **********HTML PATHS*********************************************************************************
 
-// Dependencies
-let db = require("../models")
-
 //Routes to be exported
 module.exports = (app)=>{
     app.get("/", (req, res)=>{
