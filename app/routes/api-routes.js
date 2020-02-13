f4fdb5439c4325d738ca6824498c1fd64c4ff116
MichaelRempe <michaeljrempe@gmail.com>
2020-02-12 21:50:55 -0500

    specify location of /views for handlebars /layouts

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,3 +1,20 @@
-module.exports = ()=>{
-    console.log("api-routes connected");
+// **********DATABASE CRUD OPERATIONS*******************************************************************
+
+// Dependencies
+let db = require("../models")
+
+//Routes to be exported
+module.exports = (app)=>{
+    app.get("/", (req, res)=>{
+        res.render("index",{message:"hello world"});
+    })
+
+
+    app.get("api/burgers/", (req, res)=>{
+        db.Burger.findAll({}).then((burgers)=>{
+            // res.json(burgers);
+            console.log(burgers);
+        })
+    })
+    
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express"); //Express Node Package to create back-end server/routes/control-flow
 const exphbs = require("express-handlebars");// Handle Bars - Templating Engine deliver/format data to front end
 const db = require("./app/models");// SQL - Database connection to query in-house data
+const path = require("path");
 
 //*******APP-CONFIG***********************************************************************************//
 let app = express();//Instance of Express server
@@ -9,9 +10,14 @@ let PORT = process.env.PORT || 8080; //Establish local & hosted server PORT expr
 // Sets up the Express app to handle data parsing 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
+//SHOULD BE SETTING VIEW PATH TO __dirname/app/views (right now it justlooking for __dirname/views)
+let viewsPath = path.join(__dirname, "/app", "/views")
+app.set('views', viewsPath);
 // Allows app to implement handlebars to template data
-app.engine("handlebars", exphbs({ defaultLayout: "main" })); 
+app.engine("handlebars", exphbs({ defaultLayout: "main", layoutsDir: viewsPath + '/layouts' })); 
 app.set("view engine", "handlebars");
+
+console.log(path.join(__dirname, "/app", "/views"))
 //Incorporate static directory
 app.use(express.static("./app/public"));
 
